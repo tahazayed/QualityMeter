7f88deb6f5113f27e8d87b40516288620fb04aef
taha zayed <tahazayed@gmail.com>
2017-06-12 21:58:15 +0200

    2017-06-12

---
+++ QualityMeter.Core/Models/CommonBaseBusinessEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QualityMeter.Core.Models
+{
+    public class CommonBaseBusinessEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
+
+        [ConcurrencyCheck]
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+    }
+}
---
+++ QualityMeter.Core/Models/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QualityMeter.Core.Models
+{
+    public class Subject : CommonBaseBusinessEntity
+    {
+        [Required]
+        [StringLength(500, MinimumLength = 2)]
+        [Index(IsUnique = true)]
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
