0645f0902bdeb63644e497b6f8b5654ab0aa0d1a
taha zayed <tahazayed@gmail.com>
2017-06-15 22:27:31 +0200

    ApplicationsController

---
+++ QualityMeter.Web/Controllers/ApplicationsController.cs
@@ -0,0 +1,126 @@
+﻿using QualityMeter.Core.Models;
+using QualityMeter.Core.Services;
+using QualityMeter.Infrastructure.Common.Services;
+using QualityMeter.Infrastructure.Data;
+using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace QualityMeter.Web.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private readonly ApplicationService _oApplicationService = new ApplicationService(new ApplicationsRepository(), new DebugLogger());
+
+        // GET: Applications
+        public ActionResult Index()
+        {
+            return View(_oApplicationService.GetAll(sort: "Name").ToList());
+        }
+
+        // GET: Applications/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = _oApplicationService.GetById(id.Value);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        public ActionResult Create()
+        {
+            Application application = new Application();
+            return View(application);
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Customer,CreationDate,LastUpdated,RowVersion")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                application.Id = Guid.NewGuid();
+                _oApplicationService.Add(application);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = _oApplicationService.GetById(id.Value);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Customer,CreationDate,LastUpdated,RowVersion")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                application.LastUpdated = DateTime.Now;
+                _oApplicationService.Update(application);
+                return RedirectToAction("Index");
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = _oApplicationService.GetById(id.Value);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            _oApplicationService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
