011ee5c6e2b7841495b8c21d763e352d8e4a54b4
taha zayed <tahazayed@gmail.com>
2017-06-20 15:19:09 +0200

    2017-06-20

--- QualityMeter.Core/Models/RoleGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusinessEntity
-{
-    public class RoleGroup
-    {
-        public int ID { get; set; }
-        public string GroupName { get; set; }
-        public long? LabelID { get; set; }
-    }
-}
